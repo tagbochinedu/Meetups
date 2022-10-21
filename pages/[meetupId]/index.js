665997e64ec0d71832297d003e95c72a8ff7bd6e
Tagbo Chinedu <johntagbo2@gmail.com>
2022-10-21 14:02:18 +0100

    continuation

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,7 +1,7 @@
 const MeetUpDetails = () => {
   return (
-    <section className='max-w-2xl'>
-      <img src="hello" alt="bye" className='' />
+    <section className="max-w-2xl">
+      <img src="https://www.planetware.com/wpimages/2019/09/seychelles-in-pictures-most-beautiful-places-to-visit-snorkeling-seychelles.jpg" alt="bye" className="" />
       <div>
         <h3></h3>
         <h4></h4>
@@ -10,5 +10,28 @@ const MeetUpDetails = () => {
     </section>
   );
 };
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [{ params: { meetupId: "m1" } }, { params: { meetupId: "m2" } }],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetUpData: {
+        img: "https://www.planetware.com/wpimages/2019/09/seychelles-in-pictures-most-beautiful-places-to-visit-snorkeling-seychelles.jpg",
+        id: meetupId,
+        title: "A swim in Seychelles",
+        address: "No 12 Seychelles Str, Seychelles",
+        description: "Let's Go Swimming",
+      },
+    },
+  };
+}
 
 export default MeetUpDetails;
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,35 @@
-import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
-const Home = () => {
-  const [loadedData, setLoadedData] = useState([]);
-  const DUMMY_DATA = [
-    {
-      id: "m1",
-      title: "A swim in Seychelles",
-      image:
-        "https://www.planetware.com/wpimages/2019/09/seychelles-in-pictures-most-beautiful-places-to-visit-snorkeling-seychelles.jpg",
-      address: "No 12 Seychelles Str, Seychelles",
-      description: "Let's Go Swimming",
-    },
-    {
-      id: "m2",
-      title: "Mountain Climbing in Nepal",
-      image:
-        "https://lp-cms-production.imgix.net/2021-12/shutterstockRF_667066843.jpg",
-      address: "No 13 Nepal Str, Nepal",
-      description: "Let's Go Mountain Climbing",
-    },
-    {
-      id: "m3",
-      title: "Sky Diving in Morocco",
-      image: "https://oklahomaskydiving.com/wp-content/uploads/IMG_1166-1.jpg",
-      address: "No 12 Morocco Str, Morocco",
-      description: "Let's Go Sky Diving",
-    },
-  ];
-  useEffect(() => {
-    setLoadedData(DUMMY_DATA);
-  }, []);
 
-  return <MeetupList meetups={loadedData} />;
+const DUMMY_DATA = [
+  {
+    id: "m1",
+    title: "A swim in Seychelles",
+    image:
+      "https://www.planetware.com/wpimages/2019/09/seychelles-in-pictures-most-beautiful-places-to-visit-snorkeling-seychelles.jpg",
+    address: "No 12 Seychelles Str, Seychelles",
+    description: "Let's Go Swimming",
+  },
+  {
+    id: "m2",
+    title: "Mountain Climbing in Nepal",
+    image:
+      "https://lp-cms-production.imgix.net/2021-12/shutterstockRF_667066843.jpg",
+    address: "No 13 Nepal Str, Nepal",
+    description: "Let's Go Mountain Climbing",
+  },
+  {
+    id: "m3",
+    title: "Sky Diving in Morocco",
+    image: "https://oklahomaskydiving.com/wp-content/uploads/IMG_1166-1.jpg",
+    address: "No 12 Morocco Str, Morocco",
+    description: "Let's Go Sky Diving",
+  },
+];
+
+
+const Home = (props) => {
+  return <MeetupList meetups={props.meetups} />;
 };
 
 //USED TO PRE-RENDER ON THE SERVER INSTEAD. ADVANTAGEOUS WHEN REQUESTS ARE EXPECTED FREQUENTLY
