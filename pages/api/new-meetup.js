c861c803804d03cdd27dac902d6b0f1036654be9
Tagbo Chinedu <johntagbo2@gmail.com>
2022-10-25 18:46:05 +0100

    the next step

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,23 +1,25 @@
-import { MongoClient } from "mongodb";
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const handler = async (req, res) => {
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
-   
-    const client = await MongoClient.connect(
-      "mongodb+srv://Zus:yochanan9898@meetups.foxbhxb.mongodb.net/?retryWrites=true&w=majority"
-    );
- console.log('hello')
-    const db = client.db();
 
-    const MeetUpsCollection = db.collection();
-    const result = await MeetUpsCollection.insertOne(data);
-
-    console.log(result);
-    client.close();
+    
+    const uri =
+      "mongodb+srv://Zus:yochanan9898@cluster0.2phhvt2.mongodb.net/?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverApi: ServerApiVersion.v1,
+    });
+    client.connect((err) => {
+      const collection = client.db("test").collection("devices");
+      // perform actions on the collection object
+      client.close();
+    });
 
     res.status(200).json({ message: "Meetup Created Succesfully" });
   }
-};
+}
 
-export default handler
+export default handler;
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,14 +1,15 @@
 import NewMeetUpForm from "../../components/meetups/NewMeetupForm";
 
 const NewMeetUp = () => {
-  const MeetUpHandler = async (meetUpData) => {
+   async function MeetUpHandler(meetUpData) {
+    console.log("hello");
     const response = await fetch("/api/new-meetup", {
       method: "POST",
       body: JSON.stringify(meetUpData),
-      header: { "Content-type": "application/json" },
+      header: { "Content-Type": "application/json" },
     });
     const result = await response.json();
-    console.log('hello')
+
     console.log(result);
   };
   return <NewMeetUpForm onAddMeetup={MeetUpHandler} />;
