46e871d88841dd1ddb608b275606fbfd6a1791f6
Tagbo Chinedu <johntagbo2@gmail.com>
2022-10-25 19:42:17 +0100

    server side bugs resolved

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,9 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
-async function handler(req, res) {
+function handler(req, res) {
   if (req.method === "POST") {
-    const data = req.body;
+    const data = JSON.parse(req.body);
 
-    
     const uri =
       "mongodb+srv://Zus:yochanan9898@cluster0.2phhvt2.mongodb.net/?retryWrites=true&w=majority";
     const client = new MongoClient(uri, {
@@ -12,10 +11,16 @@ async function handler(req, res) {
       useUnifiedTopology: true,
       serverApi: ServerApiVersion.v1,
     });
-    client.connect((err) => {
-      const collection = client.db("test").collection("devices");
-      // perform actions on the collection object
-      client.close();
+    client.connect(async (err) => {
+      if (err) {
+        throw err;
+      }
+      const collection = client.db().collection("meetups");
+      const result = await collection.insertOne(data);
+      console.log(result);
+      setTimeout(() => {
+        client.close();
+      }, 4000);
     });
 
     res.status(200).json({ message: "Meetup Created Succesfully" });
